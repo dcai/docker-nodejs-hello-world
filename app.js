134c71b3c5caf21f80b59f2527b6d2235004463a
Dongsheng Cai <d@tux.im>
2019-12-04 16:22:18 +1100

    fix(helm): implement LB and replicas

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || "0.0.0.0";
 const app = express();
 app.get("/", (req, res) => {
