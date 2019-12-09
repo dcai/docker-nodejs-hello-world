ec368ed001d4e4bd978c04d38d3b95cd3f83e7e0
Dongsheng Cai <d@tux.im>
2019-12-09 13:53:32 +1100

    fix(express): json response

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const os = require("os");
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || "0.0.0.0";
 const app = express();
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   const hostname = os.hostname();
-  res.send(`<html>
-        <body>
-          <h1>Hello world</h1>
-          <footer>hostname: ${hostname}</footer>
-        </body>
-        </html>`);
+  res.json({
+    data: {
+      message: "hello world",
+      hostname
+    }
+  });
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
