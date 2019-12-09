3bc39a68acedbf8d6305abef045afe1062ed8347
Dongsheng Cai <d@tux.im>
2019-12-09 12:24:52 +1100

    print hostname

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const os = require("os");
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || "0.0.0.0";
 const app = express();
 app.get("/", (req, res) => {
-  res.send("<html><body><h1>Hello world</h1></body></html>");
+  const hostname = os.hostname();
+  res.send(`<html>
+        <body>
+          <h1>Hello world</h1>
+          <footer>hostname: ${hostname}</footer>
+        </body>
+        </html>`);
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
